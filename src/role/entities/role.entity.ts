2463b8d4f723933b6708a253ae7439bdf9411a18
juzhiqinag <1020814597@qq.com>
2023-08-10 00:00:50 +0800

    feat: add role entity

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -1,1 +1,28 @@
-export class Role {}
+import { Permission } from 'src/permission/entities/permission.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'roles',
+})
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 20,
+    comment: '角色名',
+  })
+  name: string;
+
+  @ManyToMany(() => Permission)
+  @JoinTable({
+    name: 'role_permissions',
+  })
+  permissions: Permission[];
+}
