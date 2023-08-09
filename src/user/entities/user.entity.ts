22fb606a66e30096129a2ae959bba7245def9b55
juzhiqinag <1020814597@qq.com>
2023-08-09 23:59:36 +0800

    feat: add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,81 @@
-export class User {}
+import { Role } from 'src/role/entities/role.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+    name: '用户名',
+  })
+  username: string;
+
+  @Column({
+    length: 50,
+    name: '密码',
+  })
+  password: string;
+
+  @Column({
+    name: 'nick_name',
+    length: 50,
+    comment: '昵称',
+  })
+  nickName: string;
+
+  @Column({
+    comment: '邮箱',
+    length: 50,
+  })
+  email: string;
+
+  @Column({
+    comment: '头像',
+    length: 100,
+    nullable: true,
+  })
+  headPic: string;
+
+  @Column({
+    comment: '手机号',
+    length: 20,
+    nullable: true,
+  })
+  phoneNumber: string;
+
+  @Column({
+    comment: '是否冻结',
+    default: false,
+  })
+  isFrozen: boolean;
+
+  @Column({
+    comment: '是否是管理员',
+    default: false,
+  })
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createTime: Date;
+
+  @UpdateDateColumn()
+  updateTime: Date;
+
+  @ManyToMany(() => Role)
+  @JoinTable({
+    name: 'user_roles',
+  })
+  roles: Role[];
+}
